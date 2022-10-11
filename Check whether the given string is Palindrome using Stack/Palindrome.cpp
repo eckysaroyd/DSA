64fff298c731af37e979bcf66b35479ff4e2cc3f
eckysaroyd <enyato98@gmail.com>
2022-10-11 12:32:51 +0530

    insert,delete and search array elements and other program which check if string is palindrome or not

---
+++ Check whether the given string is Palindrome using Stack/Palindrome.cpp
@@ -0,0 +1,64 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function that returns true
+// if string is a palindrome
+bool isPalindrome(string s)
+{
+	int length = s.size();
+
+	// Creating a Stack
+	stack<char> st;
+
+	// Finding the mid
+	int i, mid = length / 2;
+
+	for (i = 0; i < mid; i++) {
+		st.push(s[i]);
+	}
+
+	// Checking if the length of the string
+	// is odd, if odd then neglect the
+	// middle character
+	if (length % 2 != 0) {
+		i++;
+	}
+
+	char ele;
+	// While not the end of the string
+	while (s[i] != '\0')
+	{
+		ele = st.top();
+		st.pop();
+
+	// If the characters differ then the
+	// given string is not a palindrome
+	if (ele != s[i])
+		return false;
+		i++;
+	}
+
+return true;
+}
+
+// Driver code
+int main()
+{
+	string parl;
+	cout <<"enter a word to check if is palindrome or not"<<endl;
+	cin>>parl;
+	string s=parl;;
+
+	if (isPalindrome(s)) {
+		cout << endl<<"Yes: "<<parl<<" Is Palindrome ";
+	}
+	else {
+		cout << "No: "<<parl<<" Is not Palindrome ";
+	}
+
+	return 0;
+}
+
+// This Code is Contributed by Harshit Srivastava
+
---
+++ elements in array/deletion.cpp
@@ -0,0 +1,25 @@
+//delete elements in array cpp
+#include<iostream>
+using namespace std;
+
+int main(){
+	int value,size;
+	cout<<"enter the size of array"<<endl;
+	cin>>size;
+	int arr[size];
+	for(int i=0; i<size;i++)   cin>>arr[i];
+	cout<<"enter the value you want to delete"<<endl;
+	cin>>value;
+	for(int i=0;i<=size;i++){
+		if(arr[i]==value){
+			for(int j=i;j<=size;j++){
+				arr[j]=arr[j+1];
+			}
+		}
+	}size--;
+	
+for(int i=0;i<size; i++){
+	cout<<arr[i]<<endl;
+}
+return 0;
+}
