de5202d00b2d77dc74df4e34af88c9d405cc980a
eckysaroyd <enyato98@gmail.com>
2022-10-18 12:43:57 +0530

    added data structure array

--- assignment 1 with 10 programs/3 Delete Elements from Array/deletion.cpp
+++ assignment 1 with 10 programs/3 Delete Elements from Array/deletion.cpp
@@ -7,9 +7,14 @@ int main(){
 	cout<<"enter the size of array"<<endl;
 	cin>>size;
 	int arr[size];
-	for(int i=0; i<size;i++)   cin>>arr[i];
+	
+	cout<<"enter the values of array"<<endl;
+	for(int i=0; i<size;i++)   
+	
+	cin>>arr[i];
 	cout<<"enter the value you want to delete"<<endl;
 	cin>>value;
+	cout<<"Array after deletion"<<endl;
 	for(int i=0;i<=size;i++){
 		if(arr[i]==value){
 			for(int j=i;j<=size;j++){
