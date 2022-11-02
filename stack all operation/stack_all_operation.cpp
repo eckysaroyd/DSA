9eeea3de253c64b595b20722c6790e710bbe8f17
eckysaroyd <enyato98@gmail.com>
2022-11-03 00:40:10 +0530

    stack_all_operation modified

--- stack all operation/stack_all_operation.cpp
+++ stack all operation/stack_all_operation.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-class stack (){
+class stack{
 	private :
 	int top;
 	int arr[5];
@@ -40,7 +40,7 @@ class stack (){
 		}
 		else
 		{
-			top++; //1
+			top++; // 0,1,2,3,4
 			arr[top]-=val;
 		}
 	}
@@ -48,6 +48,7 @@ class stack (){
 		if(isEmpty())
 		{
 			cout<<"Stack Underflow"<<endl;	
+			return 0;
 		}
 		else
 		{
@@ -76,7 +77,7 @@ class stack (){
 	void change(int pos, int val)
 	{
 		arr[pos]=val;
-		cout<<"Value changed at location"<<endl;	
+		cout<<"Value changed at location"<<pos<<endl;	
 	}
 	void display()
 	{
@@ -88,5 +89,74 @@ class stack (){
 	}
 };
 int main(){
+	stack s1;
+	int option,position,value;
+	do{
+		cout<<"What operation do you want to perform? Select option number. Enter 0 to exit."<<endl;
+		cout<<"1. Push"<<endl;
+		cout<<"2. Pop"<<endl;
+		cout<<"3. isEmpty()"<<endl;
+		cout<<"4. isFull()"<<endl;
+		cout<<"5. Peek()"<<endl;
+		cout<<"6. Count"<<endl;
+		cout<<"7. Change"<<endl;
+		cout<<"8. display()"<<endl;
+		cout<<"9. Clear Screen"<<endl<<endl;
+		
+		cin>>option;
+		switch(option)
+		{
+			case 0:
+				break;
+			case 1:
+				cout<<"Enter an Item to push in the stack"<<endl;
+				cin>>value;
+				s1.push(value);
+				break;
+			case 2:
+				cout<<"Pop Function called - Popped value"<<s1.pop()<<endl;
+				break;
+			case 3:
+				if(s1.isEmpty())
+				 	cout<<"Stack is Empty"<<endl;
+				else
+					cout<<"Stack is not Empty"<<endl;
+				break;
+			case 4:
+				if(s1.isFull())
+				 	cout<<"Stack is Full"<<endl;
+				else
+					cout<<"Stack is not Full"<<endl;
+				break;
+			case 5:
+				cout<<"Enter position of your item you want to peek: "<<endl;
+				cin>>position;
+				cout<<"Peek function called - Value at position "<<position<<" is "<<s1.peek(position)<<endl;
+				break;
+			case 6:
+				cout<<"Count function called - Number of items in the stack are: "<<s1.count()<<endl;
+				break;
+			case 7:
+				cout<<"Change function called - "<<endl;
+				cout<<"Enter position of your item you want to change : ";
+				cin>>position;
+				cout<<endl;
+				cout<<"Enter the value of item you want to change :";
+				cin>>value;
+				s1.change(position,value);
+				break;
+			case 8:
+				cout<<"Display function Called : "<<endl;
+				s1.display();
+				break;
+			case 9:
+				system("cls");
+				break;
+			default:
+				cout<<"Enter Proper option number"<<endl;				
+		}
+	}while(option!=0);
+	
+	return 0;
 	
 }
